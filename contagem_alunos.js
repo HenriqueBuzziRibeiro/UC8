ea5a1e5fc9d308a139f28232e1870b1027f82d65
Henrique <henriquebuzzi1997@gmail.com>
2022-06-05 19:06:45 -0300

    contagem_alunos

---
+++ contagem_alunos.js
@@ -0,0 +1,12 @@
+let numero_alunos=10;
+
+for (let i=0; i<numero_alunos; i++);
+    if(i==0){
+        console.log("O número é ZERO")
+    }else{
+        if(i%2=0){
+            console.log("O número "+i+" é par.")
+        }else{
+            console.log("O número "+i+" é impar.")
+        }
+    }
